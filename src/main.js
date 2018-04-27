e5ac7e0644df3259c718ce3ec37383f4cca037b9
cb <csbutton01@hotmail.com>
2018-04-27 13:07:20 -0700

    add node true to eslint, .env works now...

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  let key = process.env.export.apikey;
   $('#weatherLocation').click(function() {
-    let key = process.env.export.API_KEY;
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C25&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${key}`).then(function(response) {
 
     }).fail(function(error) {
