7dd229afaeac87dfb1846068fc1d9591d96fd243
cb <csbutton01@hotmail.com>
2018-04-27 15:49:02 -0700

    add styling to content

--- src/main.js
+++ src/main.js
@@ -10,18 +10,23 @@ $(document).ready(function() {
     let symptom = $('#symptom').val();
     call.apiCall(symptom).then(function(response) {
       for (let i = 0; i < `${response.data.length}`; i++){
-      $('#output').append("<li><div class='card'>" +
-       "<div class='card-title'>" +
-       `${response.data[i].practices[0].name}`+"</div>" +
-        "<ul class='card-body text'><li>" +
-       `${response.data[i].profile.first_name}` + " " +
+        $('#output').append("<li><div class='card'>" +
+        "<div class='card-header'>" +
+        `${response.data[i].practices[0].name}`+"</div>" +
+        "<ul class='card-body'><li>" +
+        "<div class='card-title'>" +
+        `${response.data[i].practices[0].visit_address.street}` + " " +
+        `${response.data[i].practices[0].visit_address.city}` + ", " +
+        `${response.data[i].practices[0].visit_address.state}` + " " +
+        `${response.data[i].practices[0].visit_address.zip}` + "<br>" +
+        `${response.data[i].profile.first_name}` + " " +
         `${response.data[i].profile.last_name}` + ", " +
-         `${response.data[i].profile.title}` + "</li>" +
-      "<li>"+
-      `${response.data[i].profile.bio}`
-       +"</li>" +
-      "</ul></div></li>"
-    );}
+        `${response.data[i].profile.title}` + "</li>" +
+        "<li class='card-text'>"+
+        `${response.data[i].profile.bio}`
+        +"</li>" +
+        "</ul></div></li>"
+      );}
     })
     .fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
